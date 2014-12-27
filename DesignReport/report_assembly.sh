af02310d673d7c9011da1d73493aa5d401617ac0
Dave Rice <dave@dericed.com>
2014-12-27 13:18:17 -0500

    report_assembly: make html and pdf versions

--- DesignReport/report_assembly.sh
+++ DesignReport/report_assembly.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+report_basename="PreformaMediainfoDesignReport"
 pwd=$(pwd)
 cd $(dirname "${0}")
-echo "" > DesignReport.md
+echo "" > "${report_basename}.md"
 for report in ../Introduction.md ../Architecture.md ../Narrative.md ../StyleGuide.md ; do
+    echo "${report}"
     if [ -f "${report}" ] ; then
-        cat "${report}" >> DesignReport.md
+        cat "${report}" >> "${report_basename}.md"
     else
         echo "Warning: ${report} is missing"
     fi
 done
+pandoc -V geometry:margin=1in -o "${report_basename}.pdf" "${report_basename}.md"
+pandoc -o "${report_basename}.html" "${report_basename}.md"
 cd "${pwd}"
