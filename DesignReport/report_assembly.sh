e96b7ff0896f1be5207c98084f016169358290d8
Dave Rice <dave@dericed.com>
2014-12-29 00:04:20 -0500

    report_assembly: add ConfCheckRegistry

--- DesignReport/report_assembly.sh
+++ DesignReport/report_assembly.sh
@@ -15,6 +15,8 @@ done
 # Report on conformance check registry
 cat ../ConformityChecks/ConformanceChecksHeader.md >> "${report_basename}.md"
 echo "" >> "${report_basename}.md"
+cat ../ConformityChecks/ConformanceCheckRegistry.md >> "${report_basename}.md"
+echo "" >> "${report_basename}.md"
 echo "## Matroska Conformance Checks (Draft)" >> "${report_basename}.md"
 cat ../ConformityChecks/ConformanceChecksMatroska.csv | csvprintf -i '### %1$s\n\n|Descriptor|Value|\n|:---------|:----|\n|CCID|%2$s|\n|Version|%3$s|\n|Authority|%4$s|\n|Target Format|%5$s|\n|Target Format Version|%6$s|\n|Target Format Part|%7$s|\n|Citation|%8$s|\n\nRule Clarity:    %10$s\n\nQuote:\n    %9$s\n\nDefinition:\n    %11$s\n\n\n'  >> "${report_basename}.md"
 
