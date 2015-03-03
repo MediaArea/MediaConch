0127750a6964a3b764b13713d21ea0712a3da644
Dave Rice <dave@dericed.com>
2015-03-02 21:17:38 -0500

    oops, fix reporting process in assembler

--- ProjectReports/report_assembly.sh
+++ ProjectReports/report_assembly.sh
@@ -45,8 +45,8 @@ echo "## Container/Stream Coherency Checks (Draft)" >> "tmp_${conformance_check_
 cat ConformityChecks/CoherencyChecks.csv | csvprintf -i '### %1$s\n\n|Descriptor|Value|\n|:---------|:----|\n|CCID|%2$s|\n|Version|%3$s|\n|Authority|%4$s|\n|Target Format|%5$s|\n|Target Format Version|%6$s|\n|Target Format Part|%7$s|\n|Citation|%8$s|\n\nRule Clarity:\n    %10$s\n\nQuote:\n    %9$s\n\nDefinition:\n    %11$s\n\n\n'  >> "tmp_${conformance_check_appendix_basename}.md"
 
 for reportbase in "${phase1_report_basename}" "${conformance_check_appendix_basename}" ; do
-    toc "tmp_${phase1_report_basename}.md"
-    pandoc -V geometry:margin=1in -o "ProjectReports/${phase1_report_basename}.pdf" "tmp_${phase1_report_basename}.md"
-    pandoc -o "ProjectReports/${phase1_report_basename}.html" "tmp_${phase1_report_basename}.md"
+    toc "tmp_${reportbase}.md"
+    pandoc -V geometry:margin=1in -o "ProjectReports/${reportbase}.pdf" "tmp_${reportbase}.md"
+    pandoc -o "ProjectReports/${reportbase}.html" "tmp_${reportbase}.md"
 done
 cd "${pwd}"
