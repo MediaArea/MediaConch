6238b47d9a9c4e81b5bc1e2a176d151c64220a00
Dave Rice <dave@dericed.com>
2015-03-02 21:25:24 -0500

    add standardization appendix to assembler

--- ProjectReports/report_assembly.sh
+++ ProjectReports/report_assembly.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-reportsuffix="MediaAreaConch_"
-phase1_report_basename="${reportsuffix}DesignReport"
+reportsuffix="MediaAreaConch"
+phase1_report_basename="${reportsuffix}_DesignReport"
 conformance_check_appendix_basename="${reportsuffix}_Appendix_ConformanceCheckRegistry"
+standardization_appendix_basename="${reportsuffix}_Appendix_Standardization"
 pwd=$(pwd)
 cd $(dirname "${0}")
 cd ..
@@ -44,7 +45,25 @@ cat ConformityChecks/ConformanceChecksLPCM.csv | csvprintf -i '### %1$s\n\n|Desc
 echo "## Container/Stream Coherency Checks (Draft)" >> "tmp_${conformance_check_appendix_basename}.md"
 cat ConformityChecks/CoherencyChecks.csv | csvprintf -i '### %1$s\n\n|Descriptor|Value|\n|:---------|:----|\n|CCID|%2$s|\n|Version|%3$s|\n|Authority|%4$s|\n|Target Format|%5$s|\n|Target Format Version|%6$s|\n|Target Format Part|%7$s|\n|Citation|%8$s|\n\nRule Clarity:\n    %10$s\n\nQuote:\n    %9$s\n\nDefinition:\n    %11$s\n\n\n'  >> "tmp_${conformance_check_appendix_basename}.md"
 
-for reportbase in "${phase1_report_basename}" "${conformance_check_appendix_basename}" ; do
+# Appendix on Standardization
+
+echo "" > "tmp_${standardization_appendix_basename}.md"
+for report in \
+    StandardsIntroduction.md \
+    StandardsNarrative.md
+do
+    if [ -f "${report}" ] ; then
+        cat "${report}" >> "tmp_${standardization_appendix_basename}.md"
+    else
+        echo "Warning: ${report} is missing"
+    fi
+done
+
+for reportbase in \
+    "${phase1_report_basename}" \
+    "${conformance_check_appendix_basename}" \
+    "${standardization_appendix_basename}"
+do
     toc "tmp_${reportbase}.md"
     pandoc -V geometry:margin=1in -o "ProjectReports/${reportbase}.pdf" "tmp_${reportbase}.md"
     pandoc -o "ProjectReports/${reportbase}.html" "tmp_${reportbase}.md"
