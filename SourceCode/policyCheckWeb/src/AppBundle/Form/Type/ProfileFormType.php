0f55ae0abc4d4aa1521a06bd0aa515b988db77cb
Guillaume Roques <guillaume.roques@gmail.com>
2015-06-05 17:30:35 +0200

    [MediaConchOnline] Small fixes to comply whith symfony 2.7 deprecated features

--- SourceCode/policyCheckWeb/src/AppBundle/Form/Type/ProfileFormType.php
+++ SourceCode/policyCheckWeb/src/AppBundle/Form/Type/ProfileFormType.php
@@ -7,25 +7,6 @@ use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
 
 class ProfileFormType extends BaseType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        parent::buildForm($builder, $options);
-/*
-        // add your custom field
-        $builder->add('firstname')
-            ->add('lastname')
-            ->add('country', 'country', array('preferred_choices' => array('GB', 'FR', 'DE', 'IT', 'SE', 'NL', 'BE', 'CH', 'US'),
-                'empty_value' => 'Choose your country',
-                'required' => false))
-            ->add('language', 'language', array('preferred_choices' => array('en_GB', 'fr', 'de', 'it', 'sv', 'nl', 'en_US'),
-                'empty_value' => 'Choose your language',
-                'required' => false))
-            ->add('professional', 'choice', array('choices' => array('u' => 'Not specified', 'y' => 'Yes', 'n' => 'No')))
-            ->add('companyName')
-            ->add('newsletter');
-*/
-    }
-
     public function getName()
     {
         return 'mediaconch_user_profile';
@@ -44,10 +25,10 @@ class ProfileFormType extends BaseType
         $builder->add('firstname')
             ->add('lastname')
             ->add('country', 'country', array('preferred_choices' => array('GB', 'FR', 'DE', 'IT', 'SE', 'NL', 'BE', 'CH', 'US', 'ES'),
-                'empty_value' => 'Choose your country',
+                'placeholder' => 'Choose your country',
                 'required' => false))
             ->add('language', 'language', array('preferred_choices' => array('en_GB', 'fr', 'de', 'it', 'sv', 'nl', 'en_US', 'es'),
-                'empty_value' => 'Choose your language',
+                'placeholder' => 'Choose your language',
                 'required' => false))
             ->add('professional', 'choice', array('choices' => array('u' => 'Not specified', 'y' => 'Yes', 'n' => 'No')))
             ->add('companyName')
