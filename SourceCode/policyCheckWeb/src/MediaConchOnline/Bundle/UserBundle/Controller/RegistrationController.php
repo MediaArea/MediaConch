38b98f40d24c8c9e7131d4371bd26508830bcf30
Guillaume Roques <guillaume.roques@gmail.com>
2015-06-22 15:31:36 +0200

    [MediaConchOnline] Fix registration with quotas

--- SourceCode/policyCheckWeb/src/MediaConchOnline/Bundle/UserBundle/Controller/RegistrationController.php
+++ SourceCode/policyCheckWeb/src/MediaConchOnline/Bundle/UserBundle/Controller/RegistrationController.php
@@ -32,9 +32,11 @@ class RegistrationController extends BaseController
 
             if ($authUser) {
                 $this->authenticateUser($user, $response);
+
+                // Set quotas for new user with mail activation disabled
+                $this->container->get('mediaconch_user.quotas')->setQuotasForNewUser();
             }
 
-            $this->container->get('mediaconch_user.quotas')->setQuotasForNewUser();
 
             return $response;
         }
@@ -42,5 +44,30 @@ class RegistrationController extends BaseController
         return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * Receive the confirmation token from user email provider, login the user
+     */
+    public function confirmAction($token)
+    {
+        $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
+        }
+
+        $user->setConfirmationToken(null);
+        $user->setEnabled(true);
+        $user->setLastLogin(new \DateTime());
+
+        $this->container->get('fos_user.user_manager')->updateUser($user);
+        $response = new RedirectResponse($this->container->get('router')->generate('fos_user_registration_confirmed'));
+        $this->authenticateUser($user, $response);
+
+        // Set quotas for new user with mail activation enabled
+        $this->container->get('mediaconch_user.quotas')->setQuotasForNewUser();
+
+        return $response;
     }
 }
