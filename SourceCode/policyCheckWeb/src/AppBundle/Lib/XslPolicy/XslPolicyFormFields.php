c5ecfec61718ffd726f074306ebf125f661e6c25
Guillaume Roques <guillaume.roques@gmail.com>
2015-10-22 19:45:43 +0200

    [MediaConchOnline] Use MediaConch to get trackTypes and fields for rule editor form

--- SourceCode/policyCheckWeb/src/AppBundle/Lib/XslPolicy/XslPolicyFormFields.php
+++ SourceCode/policyCheckWeb/src/AppBundle/Lib/XslPolicy/XslPolicyFormFields.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Lib\XslPolicy;
 
+use AppBundle\Lib\MediaConch\MediaConchTrackTypes;
+use AppBundle\Lib\MediaConch\MediaConchTrackTypeFields;
+
 class XslPolicyFormFields
 {
     public static function getValidators()
@@ -16,105 +19,43 @@ class XslPolicyFormFields
                 'exists' => 'Exists',
                 'does_not_exist' => 'Does not exist',
                 'contains_string' => 'Contains string',
-                'is_true' => 'Is true',
                 );
     }
 
     public static function getTrackTypes()
     {
-        return array(
-                'General' => 'General',
-                'Video' => 'Video',
-                'Audio' => 'Audio',
-                );
-    }
+        $mcTrackTypes = new MediaConchTrackTypes();
+        $mcTrackTypes->run();
+        $mcTrackTypes = $mcTrackTypes->getOutput();
+        $mcTrackTypes = explode(',', $mcTrackTypes);
+        $trackTypes = array();
 
-    public static function getFields($type, $xslField)
-    {
-        switch ($type) {
-            case 'General':
-                $fields = array(
-                    'ID' => 'ID',
-                    'UniqueID' => 'UniqueID',
-                    'CompleteName' => 'CompleteName',
-                    'CompleteName_Last' => 'CompleteName_Last',
-                    'Format' => 'Format',
-                    'Format_Commercial_IfAny' => 'Format_Commercial_IfAny',
-                    'Format_Version' => 'Format_Version',
-                    'Format_Profile' => 'Format_Profile',
-                    'Format_Compression' => 'Format_Compression',
-                    'Format_Settings' => 'Format_Settings',
-                    'CodecID' => 'CodecID',
-                    'Interleaved' => 'Interleaved',
-                    'FileSize' => 'FileSize',
-                    'Duration' => 'Duration',
-                    'Duration_Start' => 'Duration_Start',
-                    'Duration_End' => 'Duration_End',
-                    'OverallBitRate_Mode' => 'OverallBitRate_Mode',
-                    'OverallBitRate' => 'OverallBitRate',
-                    'OverallBitRate_Minimum' => 'OverallBitRate_Minimum',
-                    'OverallBitRate_Nominal' => 'OverallBitRate_Nominal',
-                    'OverallBitRate_Maximum' => 'OverallBitRate_Maximum',
-                    'FrameRate' => 'FrameRate',
-                    'FrameCount' => 'FrameCount',
-                    );
-            break;
-
-            case 'Video':
-                $fields = array(
-                    'ID' => 'ID',
-                    'UniqueID' => 'UniqueID',
-                    'Format' => 'Format',
-                    'Format_Commercial_IfAny' => 'Format_Commercial_IfAny',
-                    'Format_Version' => 'Format_Version',
-                    'Format_Profile' => 'Format_Profile',
-                    'MuxingMode' => 'MuxingMode',
-                    'CodecID' => 'CodecID',
-                    'Duration' => 'Duration',
-                    'BitRate_Mode' => 'BitRate_Mode',
-                    'BitRate' => 'BitRate',
-                    'Width' => 'Width',
-                    'Height' => 'Height',
-                    'PixelAspectRatio' => 'PixelAspectRatio',
-                    'DisplayAspectRatio' => 'DisplayAspectRatio',
-                    'FrameRate_Mode' => 'FrameRate_Mode',
-                    'FrameRate' => 'FrameRate',
-                    'FrameCount' => 'FrameCount',
-                    'BitDepth' => 'BitDepth',
-                    'ScanType' => 'ScanType',
-                    'ScanOrder' => 'ScanOrder',
-                    'Compression_Mode' => 'Compression_Mode',
-                    'TimeStamp_FirstFrame' => 'TimeStamp_FirstFrame',
-                    'StreamSize' => 'StreamSize',
-                    );
-            break;
-
-            case 'Audio':
-                $fields = array(
-                    'ID' => 'ID',
-                    'UniqueID' => 'UniqueID',
-                    'Format' => 'Format',
-                    'MuxingMode' => 'MuxingMode',
-                    'CodecID' => 'CodecID',
-                    'Duration' => 'Duration',
-                    'BitRate_Mode' => 'BitRate_Mode',
-                    'BitRate' => 'BitRate',
-                    'Channel(s)' => 'Channel(s)',
-                    'ChannelPositions' => 'ChannelPositions',
-                    'FrameRate' => 'FrameRate',
-                    'BitDepth' => 'BitDepth',
-                    'Compression_Mode' => 'Compression_Mode',
-                    'StreamSize' => 'StreamSize',
-                    'Language' => 'Language',
-                    );
-            break;
-
-            default:
-                $fields = array();
+        foreach ($mcTrackTypes as $type) {
+            $trackTypes[$type] = $type;
         }
 
-        if (!in_array($xslField, $fields)) {
-            $fields[$xslField] = $xslField;
+        return $trackTypes;
+    }
+
+    public static function getFields($trackType, $xslField = null)
+    {
+        $fields = array();
+
+        if ('' != $trackType) {
+            $mcFields = new MediaConchTrackTypeFields();
+            $mcFields->run($trackType);
+            $mcFields = $mcFields->getOutput();
+            $mcFields = explode(',', $mcFields);
+
+            foreach ($mcFields as $field) {
+                $fields[$field] = $field;
+            }
+        }
+
+        if ($xslField != null) {
+            if (!in_array($xslField, $fields)) {
+                $fields[$xslField] = $xslField;
+            }
         }
 
         return $fields;
